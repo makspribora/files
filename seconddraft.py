4251544e4ea4ab0b15e161c4fdd564fefa9ab9f3
makspribora <pribora2006@mail.ru>
2024-01-31 18:22:53 +0600

    second dratf and txt

---
+++ seconddraft.py
@@ -0,0 +1,11 @@
+def count_result_first():
+    # get data
+
+def count_max_math():
+    # get some
+
+with open("mat_dv.txt", "r") as file:
+        data = file.readlines()
+
+winners = count_result_first()
+print(winners)
